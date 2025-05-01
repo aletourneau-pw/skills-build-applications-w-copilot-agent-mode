0e182fcf04850c0451c392930014761654c4cb60
Lady Runa <45471155+aletourneau-pw@users.noreply.github.com>
2025-05-01 19:57:41 +0000

    Update OctoFit models for MongoDB compatibility and test data population

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+Database = "https://docs.djangoproject.com/en/4.1/ref/settings/#databases"
 
 DATABASES = {
     'default': {
